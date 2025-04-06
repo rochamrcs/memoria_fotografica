43952d82db41f36c6aa09f23f100ac40f6d407ed
rochamrcs <mavinicius95@gmail.com>
2025-04-05 22:48:39 -0300

    feat(django): implementação da funcionalide de salvar um foto na galeria

--- app/galeria/admin.py
+++ app/galeria/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from galeria.models import Fotos
 
-# Register your models here.
+
+class FotosAdmin(admin.ModelAdmin):
+    list_display = ('descricao', 'registrada_em', 'localizacao', 'upload_at')
+    search_filds = ('descricao',)
+
+admin.site.register(Fotos, FotosAdmin)
--- app/galeria/models.py
+++ app/galeria/models.py
@@ -2,4 +2,16 @@ from django.db import models
 
 
 class Fotos(models.Model):
-    ...
+    id = models.AutoField(primary_key=True)
+    image = models.ImageField(upload_to='memorias/')
+    descricao = models.TextField(blank=True)
+    registrada_em = models.DateField(null=True, blank=True)
+    localizacao = models.CharField(max_length=255, blank=True)
+    upload_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Foto"
+        verbose_name_plural = "Fotos"
+
+    def __str__(self):
+        return f"Foto {self.id}"
