777bfbebc3ce7b753f0ad8d98f1171a584579578
rochamrcs <mavinicius95@gmail.com>
2025-05-20 23:03:05 -0300

    Implementação dos albuns

--- app/galeria/admin.py
+++ app/galeria/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from galeria.models import Fotos
+from galeria.models import Fotos, Album
 
 
 class FotosAdmin(admin.ModelAdmin):
     list_display = ('descricao', 'registrada_em', 'localizacao', 'upload_at')
-    search_filds = ('descricao',)
+    search_fields = ('descricao','localizacao')
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('album_name',)
+    search_fields = ('album_name',)
 
 admin.site.register(Fotos, FotosAdmin)
+admin.site.register(Album, AlbumAdmin)
--- app/galeria/models.py
+++ app/galeria/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class Album(models.Model):
+    id = models.AutoField(primary_key=True)
+    album_name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.model
 
 class Fotos(models.Model):
     id = models.AutoField(primary_key=True)
@@ -7,6 +13,7 @@ class Fotos(models.Model):
     descricao = models.TextField(blank=True)
     registrada_em = models.DateField(null=True, blank=True)
     localizacao = models.CharField(max_length=255, blank=True)
+    album = models.ForeignKey(Album,on_delete=models.PROTECT, related_name='Album', blank=True)
     upload_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -15,3 +22,4 @@ class Fotos(models.Model):
 
     def __str__(self):
         return f"Foto {self.id}"
+
